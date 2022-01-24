2dc3d77e4fe81b50dccd72584d40a43fd56cc698
Winston <winstongu20@gmail.com>
2022-01-24 22:49:39 +0800

    Finish HW01

---
+++ labs/lab00/lab00.py
@@ -0,0 +1,19 @@
+'''
+功能模块与目的: 
+版本: 
+开发者: 高嘉伟(gaojw20@mails.tsinghua.edu.cn)
+Date: 2022-01-12 08:15:46
+版权信息: 
+最后修改者: 高嘉伟(gaojw20@mails.tsinghua.edu.cn)
+LastEditTime: 2022-01-12 08:22:26
+修改日志: 
+备注: 
+'''
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_one()
+    2021
+    """
+    return 2111 - 111 + 21
