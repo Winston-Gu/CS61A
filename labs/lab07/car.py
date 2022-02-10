1d73373c0ba3126977be3cb08f8ed58f24403e0a
Winston <winstongu20@gmail.com>
2022-02-11 02:50:09 +0800

    Finish lab07

---
+++ labs/lab07/car.py
@@ -0,0 +1,52 @@
+'''
+功能模块与目的: 
+版本: 
+开发者: 高嘉伟(gaojw20@mails.tsinghua.edu.cn)
+Date: 2022-02-11 00:43:30
+版权信息: 
+最后修改者: 高嘉伟(gaojw20@mails.tsinghua.edu.cn)
+LastEditTime: 2022-02-11 01:19:10
+修改日志: 
+备注: 
+'''
+class Car(object):
+    num_wheels = 4
+    gas = 30
+    headlights = 2
+    size = 'Tiny'
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+        self.color = 'No color yet. You need to paint me.'
+        self.wheels = Car.num_wheels
+        self.gas = Car.gas
+
+    def paint(self, color):
+        self.color = color
+        return self.make + ' ' + self.model + ' is now ' + color
+
+    def drive(self):
+        if self.wheels < Car.num_wheels or self.gas <= 0:
+            return 'Cannot drive!'
+        self.gas -= 10
+        return self.make + ' ' + self.model + ' goes vroom!'
+
+    def pop_tire(self):
+        if self.wheels > 0:
+            self.wheels -= 1
+
+    def fill_gas(self):
+        self.gas += 20
+        return 'Gas level: ' + str(self.gas)
+
+
+class MonsterTruck(Car):
+    size = 'Monster'
+
+    def rev(self):
+        print('Vroom! This Monster Truck is huge!')
+
+    def drive(self):
+        self.rev()
+        return Car.drive(self)
